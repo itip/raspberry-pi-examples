9cbce487ef53905274fde11bf73e65be72e6e23f
Ian Tipton <ian@bronzelabs.co.uk>
2015-06-07 13:13:54 +0000

    Improved output messages. Changed pull up resistor to pull down so the GPIO putput is positive when motion is detected (rather than when not detected).

--- intruder_alarm/part2 - motion detector/sensor.py
+++ intruder_alarm/part2 - motion detector/sensor.py
@@ -9,12 +9,14 @@ sensor = 4
 # rather than the logical position, e.g. 1st, 2nd, 3rd. See http://raspberrypi.stackexchange.com/a/12967
 GPIO.setmode(GPIO.BCM)
 
-# Specify which pin we're going to use. Also add a 10K pull up resistor.
+# Specify which pin we're going to use. Also add a 10K pull down resistor.
 # see http://sourceforge.net/p/raspberry-gpio-python/wiki/Inputs/
-GPIO.setup(sensor, GPIO.IN, GPIO.PUD_UP)
+GPIO.setup(sensor, GPIO.IN, GPIO.PUD_DOWN)
 
 # Create an infinite loop which repeatedly checks the input sensor/
 while True:
    time.sleep(0.1)
-   value = GPIO.input(sensor)
-   print("GPIO pin %s is %s" % (sensor, value))
+   if GPIO.input(sensor):
+       print("Motion detected")
+   else:
+      print('No motion')
