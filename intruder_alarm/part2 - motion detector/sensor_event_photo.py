f03219e5e9f0c7dcf54cf6713843f953921fc76a
Ian Tipton <ian.tipton@gmail.com>
2015-06-07 16:00:16 +0100

    Fixed typos

--- intruder_alarm/part2 - motion detector/sensor_event_photo.py
+++ intruder_alarm/part2 - motion detector/sensor_event_photo.py
@@ -11,7 +11,7 @@ sensor = 4
 # rather than the logical position, e.g. 1st, 2nd, 3rd. See http://raspberrypi.stackexchange.com/a/12967
 GPIO.setmode(GPIO.BCM)
 
-# Specify which pin we're going to use. Also add a 10K pull up resistor.
+# Specify which pin we're going to use. Also add a 10K pull down resistor.
 # see http://sourceforge.net/p/raspberry-gpio-python/wiki/Inputs/
 GPIO.setup(sensor, GPIO.IN, GPIO.PUD_DOWN)
 
